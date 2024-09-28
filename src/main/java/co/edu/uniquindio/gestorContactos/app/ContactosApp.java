2d276de8f5b64e0c7a457a7cb9e081ba23f0725b
Catacc0421 <catalina.clavijoc@uqvirtual.edu.co>
2024-09-27 23:56:18 -0500

    Se creó contactosApp y se actualizó el pom

---
+++ src/main/java/co/edu/uniquindio/gestorContactos/app/ContactosApp.java
@@ -0,0 +1,29 @@
+package co.edu.uniquindio.gestorContactos.app;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class ContactosApp extends Application {
+    @Override
+    public void start(Stage stage) throws Exception {
+
+        FXMLLoader loader = new FXMLLoader(ContactosApp.class.getResource("/inicio.fxml"));
+        Parent parent = loader.load();
+
+        Scene scene = new Scene(parent, 1000, 360);
+        stage.setScene(scene);
+        stage.setTitle("Aplicación contactos");
+        //stage.setResizable(false);
+        stage.show();
+
+    }
+
+    public static void main(String[] args) {
+        launch(ContactosApp.class, args);
+    }
+
+}
+
